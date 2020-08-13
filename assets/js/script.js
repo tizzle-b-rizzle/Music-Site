f94255582804da7305b4002d2cd603df219d3b19
Rooker <tyler.rooker@dxc.com>
2020-08-13 13:56:42 +0100

    working on js now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,12 @@
-//Do it so onclick it takes you to either the desktop or mobile depending
-//actually do it Tyler
+$ = document.getElementById
+
+function openReview(a) {
+    switch (a) {
+        case 1:
+            $("album-image").style.backgroundColor = red;
+            break;
+        default:
+            $("album-image").src = "assets/images/aviary.jpg";
+            break;
+    }
+}
