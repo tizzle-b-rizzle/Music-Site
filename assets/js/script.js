e9d267553ccec0b5d6c04877ab098c49c3788065
Rooker <tyler.rooker@dxc.com>
2020-08-14 13:38:22 +0100

    still working on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-$ = document.getElementById
+var $ = document.getElementById
 
 function openReview(a) {
     switch (a) {
         case 1:
-            $("album-image").style.backgroundColor = red;
+            document.getElementById("entire-page-container").style.backgroundColor = "red";
             break;
         default:
             $("album-image").src = "assets/images/aviary.jpg";
