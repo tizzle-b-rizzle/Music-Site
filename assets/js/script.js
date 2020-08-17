28ba61ff1e45202ab1fcd013da2445a075e31eb4
Rooker <tyler.rooker@dxc.com>
2020-08-17 14:10:14 +0100

    need to get the onclick to work for the album

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
 function openReview(a) {
     switch (a) {
         case 1:
-            cross = document.getElementById('exit-cross');
-            cross.style.backgroundColor = 'red';
-            break;
+            review = document.getElementById("album-review-page-desktop-album-1");
+            review.style.visibility = "visible";
+    }
+}
+
+function closeReview(b) {
+    switch (b) {
+        case 1:
+            review = document.getElementById("album-review-page-desktop-album-1");
+            review.style.visibility = "hidden";
         default:
-            document.getElementById("entire-page-container").style.backgroundColor = "blue";
             break;
     }
 }
