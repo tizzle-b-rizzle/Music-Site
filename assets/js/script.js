ef9d8ef2575151b23c1f36b19b4bd261a37c7ead
Rooker <tyler.rooker@dxc.com>
2020-08-17 13:48:26 +0100

    JS seems to be working, now I need to change all the IDs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
-var $ = document.getElementById
-
 function openReview(a) {
     switch (a) {
         case 1:
-            document.getElementById("entire-page-container").style.backgroundColor = "red";
+            cross = document.getElementById('exit-cross');
+            cross.style.backgroundColor = 'red';
             break;
         default:
-            $("album-image").src = "assets/images/aviary.jpg";
+            document.getElementById("entire-page-container").style.backgroundColor = "blue";
             break;
     }
 }
