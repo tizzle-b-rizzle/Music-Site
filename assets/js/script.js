8c5eca835aa1fd69588a5683d6621961e78531b4
Rooker <tyler.rooker@dxc.com>
2020-08-19 16:35:29 +0100

    adding HTMl, JS for opening desktop reviews works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-function openReview(a) {
+function openDesktopReview(a) { //function that "opens" each review on desktop
     switch (a) {
         case 1:
             review = document.getElementById("album-review-page-desktop-album-1");
@@ -8,10 +8,203 @@ function openReview(a) {
             review = document.getElementById("album-review-page-desktop-album-2");
             review.style.display = "flex";
             break;
+        case 3:
+            review = document.getElementById("album-review-page-desktop-album-3");
+            review.style.display = "flex";
+            break;
+        case 4:
+            review = document.getElementById("album-review-page-desktop-album-4");
+            review.style.display = "flex";
+            break;
+        case 5:
+            review = document.getElementById("album-review-page-desktop-album-5");
+            review.style.display = "flex";
+            break;
+        case 6:
+            review = document.getElementById("album-review-page-desktop-album-6");
+            review.style.display = "flex";
+            break;
+        case 7:
+            review = document.getElementById("album-review-page-desktop-album-7");
+            review.style.display = "flex";
+            break;
+        case 8:
+            review = document.getElementById("album-review-page-desktop-album-8");
+            review.style.display = "flex";
+            break;
+        case 9:
+            review = document.getElementById("album-review-page-desktop-album-9");
+            review.style.display = "flex";
+            break;
+        case 10:
+            review = document.getElementById("album-review-page-desktop-album-10");
+            review.style.display = "flex";
+            break;
+        case 11:
+            review = document.getElementById("album-review-page-desktop-album-11");
+            review.style.display = "flex";
+            break;
+        case 12:
+            review = document.getElementById("album-review-page-desktop-album-12");
+            review.style.display = "flex";
+            break;
+        case 13:
+            review = document.getElementById("album-review-page-desktop-album-13");
+            review.style.display = "flex";
+            break;
+        case 14:
+            review = document.getElementById("album-review-page-desktop-album-14");
+            review.style.display = "flex";
+            break;
+        case 15:
+            review = document.getElementById("album-review-page-desktop-album-15");
+            review.style.display = "flex";
+            break;
+        case 16:
+            review = document.getElementById("album-review-page-desktop-album-16");
+            review.style.display = "flex";
+            break;
+        case 17:
+            review = document.getElementById("album-review-page-desktop-album-17");
+            review.style.display = "flex";
+            break;
+        case 18:
+            review = document.getElementById("album-review-page-desktop-album-18");
+            review.style.display = "flex";
+            break;
+        case 19:
+            review = document.getElementById("album-review-page-desktop-album-19");
+            review.style.display = "flex";
+            break;
+        case 20:
+            review = document.getElementById("album-review-page-desktop-album-20");
+            review.style.display = "flex";
+            break;
+        case 21:
+            review = document.getElementById("album-review-page-desktop-album-21");
+            review.style.display = "flex";
+            break;
+        case 22:
+            review = document.getElementById("album-review-page-desktop-album-22");
+            review.style.display = "flex";
+            break;
+        case 23:
+            review = document.getElementById("album-review-page-desktop-album-23");
+            review.style.display = "flex";
+            break;
+        case 24:
+            review = document.getElementById("album-review-page-desktop-album-24");
+            review.style.display = "flex";
+            break;
+        case 25:
+            review = document.getElementById("album-review-page-desktop-album-25");
+            review.style.display = "flex";
+            break;
+        case 26:
+            review = document.getElementById("album-review-page-desktop-album-26");
+            review.style.display = "flex";
+            break;
+        case 27:
+            review = document.getElementById("album-review-page-desktop-album-27");
+            review.style.display = "flex";
+            break;
+        case 28:
+            review = document.getElementById("album-review-page-desktop-album-28");
+            review.style.display = "flex";
+            break;
+        case 29:
+            review = document.getElementById("album-review-page-desktop-album-29");
+            review.style.display = "flex";
+            break;
+        case 30:
+            review = document.getElementById("album-review-page-desktop-album-30");
+            review.style.display = "flex";
+            break;
+        case 31:
+            review = document.getElementById("album-review-page-desktop-album-31");
+            review.style.display = "flex";
+            break;
+        case 32:
+            review = document.getElementById("album-review-page-desktop-album-32");
+            review.style.display = "flex";
+            break;
+        case 33:
+            review = document.getElementById("album-review-page-desktop-album-33");
+            review.style.display = "flex";
+            break;
+        case 34:
+            review = document.getElementById("album-review-page-desktop-album-34");
+            review.style.display = "flex";
+            break;
+        case 35:
+            review = document.getElementById("album-review-page-desktop-album-35");
+            review.style.display = "flex";
+            break;
+        case 36:
+            review = document.getElementById("album-review-page-desktop-album-36");
+            review.style.display = "flex";
+            break;
+        case 37:
+            review = document.getElementById("album-review-page-desktop-album-37");
+            review.style.display = "flex";
+            break;
+        case 38:
+            review = document.getElementById("album-review-page-desktop-album-38");
+            review.style.display = "flex";
+            break;
+        case 39:
+            review = document.getElementById("album-review-page-desktop-album-39");
+            review.style.display = "flex";
+            break;
+        case 40:
+            review = document.getElementById("album-review-page-desktop-album-40");
+            review.style.display = "flex";
+            break;
+        case 41:
+            review = document.getElementById("album-review-page-desktop-album-41");
+            review.style.display = "flex";
+            break;
+        case 42:
+            review = document.getElementById("album-review-page-desktop-album-42");
+            review.style.display = "flex";
+            break;
+        case 43:
+            review = document.getElementById("album-review-page-desktop-album-43");
+            review.style.display = "flex";
+            break;
+        case 44:
+            review = document.getElementById("album-review-page-desktop-album-44");
+            review.style.display = "flex";
+            break;
+        case 45:
+            review = document.getElementById("album-review-page-desktop-album-45");
+            review.style.display = "flex";
+            break;
+        case 46:
+            review = document.getElementById("album-review-page-desktop-album-46");
+            review.style.display = "flex";
+            break;
+        case 47:
+            review = document.getElementById("album-review-page-desktop-album-47");
+            review.style.display = "flex";
+            break;
+        case 48:
+            review = document.getElementById("album-review-page-desktop-album-48");
+            review.style.display = "flex";
+            break;
+        case 49:
+            review = document.getElementById("album-review-page-desktop-album-49");
+            review.style.display = "flex";
+            break;
+        case 50:
+            review = document.getElementById("album-review-page-desktop-album-50");
+            review.style.display = "flex";
+            break;
+
     }
 }
 
-function closeReview(b) {
+function closeDesktopReview(b) {
     switch (b) {
         case 1:
             review = document.getElementById("album-review-page-desktop-album-1");
@@ -19,6 +212,10 @@ function closeReview(b) {
             break;
         case 2:
             review = document.getElementById("album-review-page-desktop-album-2");
+            review.style.display = "none";
+            break;
+        case 3:
+            review = document.getElementById("album-review-page-desktop-album-3");
             review.style.display = "none";
             break;
     }
