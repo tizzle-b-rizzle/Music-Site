4a69f8dea830bde2f911d615ab44074c6d7ee5bd
Rooker <tyler.rooker@dxc.com>
2020-08-19 13:42:09 +0100

    started doing the other reviews, still need to work out an if else

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,10 @@ function openReview(a) {
     switch (a) {
         case 1:
             review = document.getElementById("album-review-page-desktop-album-1");
-            review.style.removeProperty = ("display");
+            review.style.display = "flex";
+            break;
+        case 2:
+            review = document.getElementById("album-review-page-desktop-album-2");
             review.style.display = "flex";
             break;
     }
@@ -12,7 +15,10 @@ function closeReview(b) {
     switch (b) {
         case 1:
             review = document.getElementById("album-review-page-desktop-album-1");
-            review.style.removeProperty = ("display");
+            review.style.display = "none";
+            break;
+        case 2:
+            review = document.getElementById("album-review-page-desktop-album-2");
             review.style.display = "none";
             break;
     }
