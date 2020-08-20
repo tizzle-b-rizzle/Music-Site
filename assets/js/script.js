38c12e9636542af99b6a543e109700d41b499718
Rooker <tyler.rooker@dxc.com>
2020-08-20 15:32:53 +0100

    need to finish html then add mobile js

--- assets/js/script.js
+++ assets/js/script.js
@@ -218,5 +218,199 @@ function closeDesktopReview(b) {
             review = document.getElementById("album-review-page-desktop-album-3");
             review.style.display = "none";
             break;
+        case 4:
+            review = document.getElementById("album-review-page-desktop-album-4");
+            review.style.display = "none";
+            break;
+        case 5:
+            review = document.getElementById("album-review-page-desktop-album-5");
+            review.style.display = "none";
+            break;
+        case 6:
+            review = document.getElementById("album-review-page-desktop-album-6");
+            review.style.display = "none";
+            break;
+        case 7:
+            review = document.getElementById("album-review-page-desktop-album-7");
+            review.style.display = "none";
+            break;
+        case 8:
+            review = document.getElementById("album-review-page-desktop-album-8");
+            review.style.display = "none";
+            break;
+        case 9:
+            review = document.getElementById("album-review-page-desktop-album-9");
+            review.style.display = "none";
+            break;
+        case 10:
+            review = document.getElementById("album-review-page-desktop-album-10");
+            review.style.display = "none";
+            break;
+        case 11:
+            review = document.getElementById("album-review-page-desktop-album-11");
+            review.style.display = "none";
+            break;
+        case 12:
+            review = document.getElementById("album-review-page-desktop-album-12");
+            review.style.display = "none";
+            break;
+        case 13:
+            review = document.getElementById("album-review-page-desktop-album-13");
+            review.style.display = "none";
+            break;
+        case 14:
+            review = document.getElementById("album-review-page-desktop-album-14");
+            review.style.display = "none";
+            break;
+        case 15:
+            review = document.getElementById("album-review-page-desktop-album-15");
+            review.style.display = "none";
+            break;
+        case 16:
+            review = document.getElementById("album-review-page-desktop-album-16");
+            review.style.display = "none";
+            break;
+        case 17:
+            review = document.getElementById("album-review-page-desktop-album-17");
+            review.style.display = "none";
+            break;
+        case 18:
+            review = document.getElementById("album-review-page-desktop-album-18");
+            review.style.display = "none";
+            break;
+        case 19:
+            review = document.getElementById("album-review-page-desktop-album-19");
+            review.style.display = "none";
+            break;
+        case 19:
+            review = document.getElementById("album-review-page-desktop-album-19");
+            review.style.display = "none";
+            break;
+        case 20:
+            review = document.getElementById("album-review-page-desktop-album-20");
+            review.style.display = "none";
+            break;
+        case 21:
+            review = document.getElementById("album-review-page-desktop-album-21");
+            review.style.display = "none";
+            break;
+        case 22:
+            review = document.getElementById("album-review-page-desktop-album-22");
+            review.style.display = "none";
+            break;
+        case 23:
+            review = document.getElementById("album-review-page-desktop-album-23");
+            review.style.display = "none";
+            break;
+        case 24:
+            review = document.getElementById("album-review-page-desktop-album-24");
+            review.style.display = "none";
+            break;
+        case 25:
+            review = document.getElementById("album-review-page-desktop-album-25");
+            review.style.display = "none";
+            break;
+        case 26:
+            review = document.getElementById("album-review-page-desktop-album-26");
+            review.style.display = "none";
+            break;
+        case 27:
+            review = document.getElementById("album-review-page-desktop-album-27");
+            review.style.display = "none";
+            break;
+        case 28:
+            review = document.getElementById("album-review-page-desktop-album-28");
+            review.style.display = "none";
+            break;
+        case 29:
+            review = document.getElementById("album-review-page-desktop-album-29");
+            review.style.display = "none";
+            break;
+        case 30:
+            review = document.getElementById("album-review-page-desktop-album-30");
+            review.style.display = "none";
+            break;
+        case 31:
+            review = document.getElementById("album-review-page-desktop-album-31");
+            review.style.display = "none";
+            break;
+        case 32:
+            review = document.getElementById("album-review-page-desktop-album-32");
+            review.style.display = "none";
+            break;
+        case 33:
+            review = document.getElementById("album-review-page-desktop-album-33");
+            review.style.display = "none";
+            break;
+        case 34:
+            review = document.getElementById("album-review-page-desktop-album-34");
+            review.style.display = "none";
+            break;
+        case 35:
+            review = document.getElementById("album-review-page-desktop-album-35");
+            review.style.display = "none";
+            break;
+        case 36:
+            review = document.getElementById("album-review-page-desktop-album-36");
+            review.style.display = "none";
+            break;
+        case 37:
+            review = document.getElementById("album-review-page-desktop-album-37");
+            review.style.display = "none";
+            break;
+        case 38:
+            review = document.getElementById("album-review-page-desktop-album-38");
+            review.style.display = "none";
+            break;
+        case 39:
+            review = document.getElementById("album-review-page-desktop-album-39");
+            review.style.display = "none";
+            break;
+        case 40:
+            review = document.getElementById("album-review-page-desktop-album-40");
+            review.style.display = "none";
+            break;
+        case 41:
+            review = document.getElementById("album-review-page-desktop-album-41");
+            review.style.display = "none";
+            break;
+        case 42:
+            review = document.getElementById("album-review-page-desktop-album-42");
+            review.style.display = "none";
+            break;
+        case 43:
+            review = document.getElementById("album-review-page-desktop-album-43");
+            review.style.display = "none";
+            break;
+        case 44:
+            review = document.getElementById("album-review-page-desktop-album-44");
+            review.style.display = "none";
+            break;
+        case 45:
+            review = document.getElementById("album-review-page-desktop-album-45");
+            review.style.display = "none";
+            break;
+        case 46:
+            review = document.getElementById("album-review-page-desktop-album-46");
+            review.style.display = "none";
+            break;
+        case 47:
+            review = document.getElementById("album-review-page-desktop-album-47");
+            review.style.display = "none";
+            break;
+        case 48:
+            review = document.getElementById("album-review-page-desktop-album-48");
+            review.style.display = "none";
+            break;
+        case 49:
+            review = document.getElementById("album-review-page-desktop-album-49");
+            review.style.display = "none";
+            break;
+        case 50:
+            review = document.getElementById("album-review-page-desktop-album-50");
+            review.style.display = "none";
+            break;
+
+
     }
 }
